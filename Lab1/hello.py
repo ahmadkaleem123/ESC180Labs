aec55de8e3041323869b6a8e4a7e2a5db1ab83e3
Ahmad Kaleem <muhammad.ahmad.kaleem@gmail.com>
2020-12-03 11:28:01 -0500

    Add files via upload

--- Lab1/hello.py
+++ Lab1/hello.py
@@ -1,1 +1,17 @@
+#print("Hello,Ahmad Kaleem")
+#print("Hello, Andy Liu")
+name1 = "Ahmad Kaleem"
+name2 = "Andy Liu"
 
+print("Hello, " + name1 + " and " + name2 + ". Your names are " + name1 + " and " + name2 +". Hi there. Your names are still " + name1 + " and " + name2 + ".")
+
+name1 = "Prof. Cluett"
+name2 = "Prof. Thywissen"
+
+
+greetee = "Lord Voldemort"
+
+if(greetee == "Lord Voldemort"):
+    print("I'm not talking to you")
+else:
+    print("Hello "+ greetee)
